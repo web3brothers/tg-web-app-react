c37c8367d3c4ed9fd4e7c17d7be83134c0d9168c
Vitaliy Hrechko <vitalii.hrechko@stripo.email>
2024-12-15 15:00:20 +0200

    fix header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { useEffect } from 'react';
 import './App.css';
-
-const tg = window.Telegram.WebApp;
+import { useTelegram } from './hooks/useTelegram';
 
 function App() {
+
+  const { tg } = useTelegram();
 
   useEffect(() => {
     tg.ready();
   }, []);
-
-
 
 
   return (
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,15 @@
 import Button from "../Button/Button";
+import { useTelegram } from "../../../hooks/useTelegram";
 
 const Header = () => {
 
-    const tg = window.Telegram.WebApp;
-    const close = () => {
-        tg.close();
-      }
-
+    const { onClose, user } = useTelegram();
 
     return (
         <div className={'header'}>
-            <Button onClick={close}>Close</Button>
+            <Button onClick={onClose}>Close</Button>
             <span className={'username'}>
-                {tg.initDataUnsafe?.user?.username}
+                {user?.username}
             </span>
         </div>
     );
