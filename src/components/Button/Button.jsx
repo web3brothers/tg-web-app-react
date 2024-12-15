5fbc3099ac64f2d751279d5e6759b20b0c5d9c7c
Vitaliy Hrechko <vitalii.hrechko@stripo.email>
2024-12-15 16:35:20 +0200

    ddd

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import './Button.css';
 import React from 'react';
 
 const Button = (props) => {
-    return <button {...props} className={'button' + props.className}>
+    return <button {...props} className={'button' + (props.className ? '' + props.className : '')}>
         
     </button>;
 }
